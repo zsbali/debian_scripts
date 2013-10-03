354f00e5c45f24ca29549b18d34c952e56a549e8
root <root@debian.virtualbox>
2013-10-03 01:45:12 -0400

    configcheck.sh added

---
+++ configcheck.sh
@@ -0,0 +1,67 @@
+#!/bin/sh
+
+printf "curl\t\t"
+    if [ `which curl` ]; then
+	which curl
+    else
+	echo ""
+    fi
+
+printf "ruby\t\t"
+    if [ `which ruby` ]; then
+	ruby -v
+    else
+	echo ""
+    fi
+
+printf "rails\t\t"
+    if [ `which rails` ]; then
+	rails -v
+    else
+	echo ""
+    fi
+
+printf "rake\t\t"
+    if [ `which rake` ]; then
+	which rake
+    else
+	ehco ""
+    fi
+
+printf "psql\t\t"
+    if [ `which psql` ]; then
+	psql --version | grep psql
+    else
+	echo ""
+    fi
+
+printf "git\t\t"
+    if [ `which git` ]; then
+	git version
+    else
+	echo ""
+    fi
+
+printf "heroku\t\t"
+    if [ `which heroku` ]; then
+	heroku --version
+    else
+	echo ""
+    fi
+
+printf "nodejs\t\t"
+    if [ `which nodejs` ]; then
+	nodejs --version
+    else
+	echo ""
+    fi
+
+printf "gem\t\t"
+    if [ `which gem` ]; then
+	gem --version
+    else
+	echo ""
+    fi
+
+echo ""
+
