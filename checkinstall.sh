37458de64123b4abdbed5a074df8708590242007
root <root@debian>
2013-10-27 15:33:36 +0100

    update

--- checkinstall.sh
+++ checkinstall.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 FW1=16
-FW2=20
+#FW2=20
+FW2=0
 COLS=`tput cols`
 FW3="`echo $COLS-$FW1-$FW2 | bc`"
 
@@ -11,26 +12,26 @@ FIELD_3_FORMAT="%-*s\n"
 
 function print3 {
     printf "$FIELD_1_FORMAT" $FW1 $1
-	if [ `echo "$2" | sh` ]; then
-	    printf "$FIELD_2_FORMAT" $FW2 `echo "$2" |sh`
-	    trimmed=`echo "$3" | sh | cut -c1-"$FW3"`
+	if [ `echo "which $1" | sh` ]; then
+	    #printf "$FIELD_2_FORMAT" $FW2 `echo "which $1" |sh`
+	    trimmed=`echo "$2" | sh | cut -c1-"$FW3"`
 	    printf "%s\n" "$trimmed"
 	else
 	    echo ""
 	fi
 }
-print3 "sudo" 	"which sudo"
-print3 "sshd" 	"which sshd"
-print3 "curl" 	"which curl"
-print3 "mc" 	"which mc"
-print3 "ruby" 	"which ruby" 	"ruby --version"
-print3 "rails" 	"which rails" 	"rails -v"
-print3 "rake" 	"which rake"
-print3 "psql" 	"which psql" 	"psql --version | grep psql"
-print3 "git" 	"which git" 	"git version"
-print3 "heroku" "which heroku" "heroku --version"
-print3 "nodejs" "which nodejs" "nodejs --version"
-print3 "gem" 	"which gem"
-print3 "nginx" "which nginx" 	"nginx -v 2>&1"
-print3 "gcc" 	"which gcc" 	"gcc --version | grep gcc"
-print3 "passenger" "which passenger" "passenger -v | grep version"
+print3 "sudo"		"sudo -V | grep 'Sudo version'" 
+print3 "sshd" 		"ssh -v 2>&1 | grep 'SSH'"
+print3 "curl" 		"curl --version | grep curl"
+print3 "mc" 		"mc --version | grep Midnight"
+print3 "ruby" 		"ruby --version"
+print3 "rails" 		"rails -v"
+print3 "rake" 		"rake --version"
+print3 "psql"  		"psql --version | grep psql"
+print3 "git"  		"git version"
+print3 "heroku"  	"heroku --version"
+print3 "nodejs"  	"nodejs --version"
+print3 "gem"		"gem -v"
+print3 "nginx" 		"nginx -v 2>&1"
+print3 "gcc" 		"gcc --version | grep gcc"
+print3 "passenger" 	"passenger -v | grep version"
