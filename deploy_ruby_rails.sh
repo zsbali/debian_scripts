740edaaaefdf525d0b6f82a2207551babae6b19b
root <root@debian>
2013-10-27 20:51:20 +0100

    update

--- deploy_ruby_rails.sh
+++ deploy_ruby_rails.sh
@@ -1,8 +1,9 @@
-#!/bin/sh
+#!/bin/bash
 apt-get update
 apt-get upgrade
 ./install_sudo.sh
 ./install_curl.sh
 ./install_ruby.sh
+source /usr/local/rvm/scripts/rvm
 ./install_rails.sh
 ./checkinstall.sh
