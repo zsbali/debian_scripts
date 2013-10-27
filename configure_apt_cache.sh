020626a907f1f716d07062fb410323eb6d1d119b
root <root@debian.virtualbox>
2013-10-27 11:42:33 -0400

    update

--- configure_apt_cache.sh
+++ configure_apt_cache.sh
@@ -1,3 +1,11 @@
+if [ $1 ]
+then
+    IP=$1
+else
+    IP="192.168.1.104"
+fi
 cat > /etc/apt/apt.conf.d/01proxy << EOF
-Acquire::http::Proxy "http://192.168.1.104:3142";
+Acquire::http::Proxy "$IP:3142";
 EOF
+wget -O- "http://$IP:3142"
+
