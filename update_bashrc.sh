cbdbcb711b28f8dd6d9b75c4c122bc6b72b31615
root <root@debian>
2013-12-09 20:33:02 +0100

    update

--- update_bashrc.sh
+++ update_bashrc.sh
@@ -1,6 +1,7 @@
-cat << EOF >> .bashrc
+cat << EOF >> ~/.bashrc
 export HISTSIZE=100000000
 export HISTFILESIZE=100000000
 export HISTIGNORE='&:ls:[bf]g:exit:[ \t]*'
 export HISTCONTROL=ignoredumps
 EOF
+
