bd9e3b4122eb140c51cc376893597f8abcbcdeff
root <root@debian>
2014-01-08 19:37:05 +0100

    update

--- install_nginx_passenger.sh
+++ install_nginx_passenger.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 gem install passenger
-passenger-install-nginx-module | tee passenger_out.txt
+passenger-install-nginx-module --auto --prefix=/opt/nginx --auto-download --language ruby,nodejs | tee passenger_out.txt
+ln -s /opt/nginx/sbin/nginx /usr/local/sbin
+
