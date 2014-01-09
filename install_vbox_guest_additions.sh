3a386afe675caaf5dc48f376a92ab07c34d14526
root <root@debian>
2014-01-09 07:38:32 +0100

    update

--- install_vbox_guest_additions.sh
+++ install_vbox_guest_additions.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 apt-get install build-essential module-assistant -y
 m-a prepare
-mount /dev/sr0 /media/cdrom
-sh /media/cdrom/VBoxLinuxAdditions.run
+#mount /dev/sr0 /media/cdrom
+sh /media/cdrom/VBoxLinuxAdditions.run || echo "\nyou have to mount /media/cdrom first!"
 
