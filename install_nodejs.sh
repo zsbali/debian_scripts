7a01770ce0db89d00f48062e7eac89fce12c75a9
Balázs Zsély <balazs.zsely@hotmail.com>
2014-01-20 20:34:19 +0100

    update

--- install_nodejs.sh
+++ install_nodejs.sh
@@ -4,6 +4,6 @@ mkdir ~/node_js_src && cd $_
 wget -N http://nodejs.org/dist/node-latest.tar.gz
 tar xzvf node-latest.tar.gz && cd node-v*
 ./configure
-checkinstall
+sudo checkinstall
 sudo dpkg -i node_*
 
