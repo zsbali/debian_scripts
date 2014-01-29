4da63fbd32125df1313ff25d5f83362ddaef140d
Balázs Zsély <balazs.zsely@hotmail.com>
2014-01-29 20:56:50 +0100

    update

---
+++ install_webmin.sh
@@ -0,0 +1,7 @@
+#/bin/bash
+
+cd ~
+wget http://prdownloads.sourceforge.net/webadmin/webmin_1.670_all.deb || exit 0
+sudo apt-get install perl libnet-ssleay-perl openssl libauthen-pam-perl libpam-runtime libio-pty-perl apt-show-versions python -y
+sudo apt-get -f install -y
+sudo dpkg --install sudo ./webmin_1.670_all.deb
