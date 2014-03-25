18338c5d66fae9592c4d50f958419c5e95da43d2
zsbali <zsely.balazs@hotmail.com>
2014-03-25 17:11:59 +0100

    Update install_java.sh

--- install_java.sh
+++ install_java.sh
@@ -1,38 +1,36 @@
 #!/bin/bash
 
-platf=$1
+platform=$1
 
-if [[ ($platf != "32") && ($platf != "64") ]]; then
+if [[ ($platform != "32") && ($platform != "64") ]]; then
     echo "usage: ./install_java 32|64"
     exit 0
 fi
 
-if [ $platf = 32 ]; then
-    if [ ! -f /tmp/jdk-*-linux-i586.tar.gz ]; then
-        wget --no-check-certificate --no-cookies - --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/7u51-b13/jdk-7u51-linux-i586.tar.gz -O /tmp/jdk-7u51-linux-i586.tar.gz --no-check-certificate || exit 0
-    fi
+LINK_32=http://download.oracle.com/otn-pub/java/jdk/7u51-b13/jdk-7u51-linux-i586.tar.gz
+LINK_64=http://download.oracle.com/otn-pub/java/jdk/7u51-b13/jdk-7u51-linux-x64.tar.gz
 
-    if [ ! -d /opt/java-oracle ]; then
-	sudo mkdir /opt/java-oracle || exit 0
-    fi
-    
-    sudo tar -zxf /tmp/jdk-*-linux-i586.tar.gz -C /opt/java-oracle || exit 0
+if [ $platform = 32 ]; then
+	LINK=$LINK_32
 else
-    if [ ! -f /tmp/jdk-*-linux-i586.tar.gz ]; then
-        wget --no-check-certificate --no-cookies - --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/7u51-b13/jdk-7u51-linux-x64.tar.gz -O /tmp/jdk-7u51-linux-i586.tar.gz --no-check-certificate || exit 0
-    fi
-
-    if [ ! -d /opt/java-oracle ]; then
-	sudo mkdir /opt/java-oracle
-    fi
-    
-    sudo tar -zxf /tmp/jdk-*-linux-i586.tar.gz -C /opt/java-oracle || exit 0
+	LINK=$LINK_64
 fi
+
+if [ ! -f /tmp/jdk-*-linux-i586.tar.gz ]; then
+	wget --no-check-certificate --no-cookies - --header "Cookie: oraclelicense=accept-securebackup-cookie" $LINK -O /tmp/jdk-7u51-linux-i586.tar.gz --no-check-certificate || exit 0
+fi
+
+if [ ! -d /opt/java-oracle ]; then
+	sudo mkdir /opt/java-oracle || echo "mkdir /opt/java-oarcle FAILED" ; exit 0
+fi
+    
+sudo tar -zxf /tmp/jdk-*-linux-i586.tar.gz -C /opt/java-oracle || exit 0
 
 JHome=/opt/java-oracle/jdk1.7.0_51
 sudo update-alternatives --install /usr/bin/java java ${JHome%*/}/bin/java 20000
 sudo update-alternatives --install /usr/bin/javac javac ${JHome%*/}/bin/javac 20000
 sudo update-alternatives --config java
+
 #if [ ! -d /opt/google/chrome/plugins ]; then
 #    sudo mkdir /opt/google/chrome/plugins || exit 0
 #    if [ $platf = 64 ]; then
