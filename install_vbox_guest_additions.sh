09305322088cac9fbfb04dffe1cbabec126484a8
zsbali <zsely.balazs@hotmail.com>
2014-03-26 14:22:36 +0100

    Update install_vbox_guest_additions.sh

--- install_vbox_guest_additions.sh
+++ install_vbox_guest_additions.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-ls /media/cdrom/VBoxLinuxAdditions.run || echo "\n You have to mount 1st!\n" && exit 0
+ls /media/cdrom/VBoxLinuxAdditions.run || { echo "\n You have to mount 1st!\n" ; exit 1; }
 apt-get install build-essential module-assistant -y
 m-a prepare
 sh /media/cdrom/VBoxLinuxAdditions.run
