3b25b036f985ac3af4de978e6e548875f9fa3790
zsbali <zsely.balazs@hotmail.com>
2014-03-27 18:54:05 +0100

    Create install_chrome.sh

---
+++ install_chrome.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+platform=$1
+
+if [[ ($platform != "32") && ($platform != "64") ]]; then
+    echo "usage: ./install_chrome 32|64"
+    exit 0
+fi
+
+LINK_32="https://dl.google.com/linux/direct/google-chrome-stable_current_i386.deb"
+LINK_64="https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb"
+SAVED_FILE="/tmp/google-chrome-stable_current.deb"
+
+if [ $platform = 32 ]; then
+	LINK=$LINK_32
+else
+	LINK=$LINK_64
+fi
+
+if [ ! -f $SAVED_FILE ]; then
+	wget $LINK -O $SAVED_FILE
+fi
+
+sudo apt-get install libxss1
+sudo dpkg -i $SAVED_FILE
+
+echo "SUCCESS!!"
