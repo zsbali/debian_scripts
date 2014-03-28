bb25e6669a43466607819e0f7de6a93c36a7f330
zsbali <zsely.balazs@hotmail.com>
2014-03-28 08:02:05 +0100

    Update install_nodejs.sh

--- install_nodejs.sh
+++ install_nodejs.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo apt-get install python g++ make checkinstall
+sudo apt-get install python g++ make checkinstall -y
 mkdir ~/node_js_src && cd $_
 wget -N http://nodejs.org/dist/node-latest.tar.gz
 tar xzvf node-latest.tar.gz && cd node-v*
