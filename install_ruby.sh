d13d885431102f43d602f406dd069676661df817
zsbali <zsely.balazs@hotmail.com>
2014-05-26 08:40:47 +0200

    Update install_ruby.sh

--- install_ruby.sh
+++ install_ruby.sh
@@ -1,5 +1,8 @@
 curl -L https://get.rvm.io | bash -s stable --ruby=2.1.1
 source ~/.rvm/scripts/rvm
+
+# In rare cases the 1st line (curl -L ...) doesn't install ruby.
+# Then you can do it by this:
 rvm install 2.1.1
 
 
