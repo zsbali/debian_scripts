b128824d329efd2bcc7c62c34ed43afb069821c8
zsbali <zsely.balazs@hotmail.com>
2014-05-26 08:58:21 +0200

    Update install_ruby.sh

--- install_ruby.sh
+++ install_ruby.sh
@@ -1,5 +1,8 @@
 curl -L https://get.rvm.io | bash -s stable --ruby=2.1.1
-source ~/.rvm/scripts/rvm
+
+# rvm and ruby will be available only after reading the rvm init script (below), but since 
+# it is read at login from .bashrc this line has o effect:
+# source ~/.rvm/scripts/rvm
 
 # In rare cases the 1st line (curl -L ...) doesn't install ruby.
 # Then you can do it by this:
