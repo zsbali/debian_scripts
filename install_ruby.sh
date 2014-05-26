c8cb283a32185fb42536cf5779921bc37c74275c
zsbali <zsely.balazs@hotmail.com>
2014-05-26 08:59:35 +0200

    Update install_ruby.sh

--- install_ruby.sh
+++ install_ruby.sh
@@ -1,7 +1,9 @@
 curl -L https://get.rvm.io | bash -s stable --ruby=2.1.1
 
-# rvm and ruby will be available only after reading the rvm init script (below), but since 
-# it is read at login from .bashrc this line has o effect:
+# rvm and ruby will be available only after reading the rvm init script (below) 
+# since it is read at login from .bashrc this line you have to logoff-login
+#
+# (sourcing only has effect if you tyoe it manually in the terminal)
 # source ~/.rvm/scripts/rvm
 
 # In rare cases the 1st line (curl -L ...) doesn't install ruby.
