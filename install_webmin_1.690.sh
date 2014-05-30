3457f82b3161b85db7abc123a33f8a2bbae35e06
zsbali <zsely.balazs@hotmail.com>
2014-05-30 19:56:51 +0200

    Update install_webmin_1.690.sh

--- install_webmin_1.690.sh
+++ install_webmin_1.690.sh
@@ -3,4 +3,4 @@ cd ~
 wget http://prdownloads.sourceforge.net/webadmin/webmin_1.690_all.deb || exit 0
 sudo apt-get install perl libnet-ssleay-perl openssl libauthen-pam-perl libpam-runtime libio-pty-perl apt-show-versions python -y
 sudo apt-get -f install -y
-sudo dpkg --install sudo ./webmin_1.690_all.deb
+sudo dpkg --install ./webmin_1.690_all.deb
