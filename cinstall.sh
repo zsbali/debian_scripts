733447af8b245734b6df8a7f7cbe97e3b23ac2f5
zsbali <zsely.balazs@hotmail.com>
2014-05-30 19:37:53 +0200

    Update cinstall.sh

--- cinstall.sh
+++ cinstall.sh
@@ -22,7 +22,7 @@ function print3 {
 }
 
 function webmin {
-	if [ dpkg -l webmin 2> /dev/null ]; then 
+	if [ `dpkg -l webmin 2> /dev/null` ]; then 
 	   #printf "$FIELD_2_FORMAT" $FW2 `echo "which $1" |sh`
 	   trimmed=`echo "$2" | sh | cut -c1-"$FW3"`
 	   printf "%s\n" "$trimmed"
