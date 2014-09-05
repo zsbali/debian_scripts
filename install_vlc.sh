c64034eb42c132dc9280dcea9ab7f921a6c1d898
zsbali <zsely.balazs@hotmail.com>
2014-09-05 23:14:18 +0200

    Delete install_vlc.sh

--- install_vlc.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if grep "deb http://http.debian.net/debian wheezy-backports main" /etc/apt/sources.list;
-then echo "";
-else sudo sh -c "echo 'deb http://http.debian.net/debian wheezy-backports main' >> /etc/apt/sources.list" ;
-fi
-
-sudo apt-get update
-sudo apt-get -t wheezy-backports install vlc -y
